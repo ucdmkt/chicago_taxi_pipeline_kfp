b66ba412e1aed35b52d2db67df37e22da119ba01
Makoto Uchida <muchida@google.com>
2019-05-24 12:21:01 -0700

    add example_validator, and other component skeletons

---
+++ templates/chicago_taxi/data_source/example_generator.py
@@ -0,0 +1,55 @@
+"""Defines skeleton of ExampleGen component."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl import app
+from absl import flags
+
+from tfx.components.example_gen.big_query_example_gen import component as big_query_example_gen_component
+from tfx.proto import example_gen_pb2
+
+from common.adapter import TfxComponentWrapper
+
+from typing import Optional, Dict, Text
+
+### NOTE: This is vulnerable to SQL Injection.
+QUERY_TMPL = """
+    SELECT
+      pickup_community_area,
+      fare,
+      EXTRACT(MONTH FROM trip_start_timestamp) AS trip_start_month,
+      EXTRACT(HOUR FROM trip_start_timestamp) AS trip_start_hour,
+      EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS trip_start_day,
+      UNIX_SECONDS(trip_start_timestamp) AS trip_start_timestamp,
+      pickup_latitude,
+      pickup_longitude,
+      dropoff_latitude,
+      dropoff_longitude,
+      trip_miles,
+      pickup_census_tract,
+      dropoff_census_tract,
+      payment_type,
+      company,
+      trip_seconds,
+      dropoff_community_area,
+      tips
+    FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+    LIMIT %s
+"""
+
+def bigquery(
+    num_records: int,
+    **kwargs
+) -> TfxComponentWrapper:
+  """Defines data generation from BigQuery."""
+
+  class _BigQueryExampleGen(TfxComponentWrapper):
+
+    def __init__(self):
+      component = big_query_example_gen_component.BigQueryExampleGen(
+          QUERY_TMPL.format(num_records))
+      super().__init__(component)
+
+  return _BigQueryExampleGen()
---
+++ templates/chicago_taxi/data_source/example_generator_test.py
@@ -0,0 +1,19 @@
+"""Tests for example_generator."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+import example_generator
+
+
+class ExampleGeneratorTest(tf.test.TestCase):
+
+  def testExampleGeneratorOp(self):
+    pass
+
+
+if __name__ == '__main__':
+  tf.test.main()
